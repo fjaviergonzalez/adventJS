b9ae0ae942abfc8b5b0654246fdb4cd4639066d4
fjaviergonzalez <72889323+fjaviergonzalez@users.noreply.github.com>
2021-12-15 23:53:42 +0100

    Solucionado problema espacios en blanco

--- dia2/regalos.js
+++ dia2/regalos.js
@@ -1,18 +1,22 @@
 export default function listGifts(letter) {
-    // ¡Tú puedes!
+    //usamos trim() para eliminar espacios en blanco al inicio y al final del string de juguetes.
+    //usamos replace(/\s+/g, ' ') para sustituir 1 o varios espacios seguidos, por un único espacio.
+    //mediante split(' ') convertimos el string a un array, tomando como separación un espacio en blanco.
+    //con filter(regalo => !regalo.startsWith("_")) eliminamos cualquier regalo tachado (comienza por "_")
+    let arrayCarta = letter.trim().replace(/\s+/g, ' ').split(' ').filter(regalo => !regalo.startsWith("_"));
+    //console.log(arrayCarta);
 
-    let cartaSinTachados = letter.replace(/\s+_[a-z]+/g, '');
-    console.log(cartaSinTachados);
-    //let cartaSoloUnEspacio = cartaSinTachados.replace(/\s+/g, ' ');
-    //console.log(cartaSoloUnEspacio);
-
-    let arrayCarta = cartaSinTachados.split(' ');
-    console.log(arrayCarta);
     let regalos = {};
+    //Si un juguete no existía en el objeto "regalos", se cuenta como la primera vez que aparece(1).
+    //Si el juguete ya existía, incrementa el contador de veces que ha aparecido (regalos[element]++).
     arrayCarta.forEach(element => {
         (regalos[element] !== undefined) ? regalos[element]++ : regalos[element] = 1;
     });
+    //console.log(regalos);
+    return regalos;
+
 }
 
-const carta = 'bici coche balón _playstation bici coche peluche';
+//const carta = 'bici coche balón _playstation bici coche peluche';
+const carta = '       bici        coche               balón     _playstation bici coche     peluche     ';
 const regalos = listGifts(carta);
